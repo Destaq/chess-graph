8a59065a238b06b9dd37c7c7bdd0fd8a6f7bf4c6
Simon <trilogicworlds@gmail.com>
2020-04-26 17:48:00 +0200

    fixed labels, parents

--- chessburst.py
+++ chessburst.py
@@ -2,9 +2,9 @@ import plotly.graph_objects as go
 import chess.pgn, time, random
 from collections import Counter
 
-mine = open('last50.pgn')
-mine_short = open('last5.pgn')
-wonder = open('Carlsen.pgn')
+mine = open('pgns/last50.pgn')
+mine_short = open('pgns/last5.pgn')
+wonder = open('pgns/Carlsen.pgn')
 
 def create_game_list(pgn):
     ''' we have one massive pgn that we convert to a list of normal game pgns '''
@@ -49,34 +49,55 @@ def form_values():
 
     first3 = [lst[i][:3] for i in range(len(lst))]
 
-    secondmove = list(Counter([tuple(first3[i][:2]) for i in range(len(lst)) if first3[i][0] == 'e4']).items())
-    thirdmove = list(Counter([tuple(first3[i][:2]) for i in range(len(lst)) if first3[i][0] == 'd4']).items())
-    rids = []
+    rids, zids = [], []
     counter = 0
     acceps = ['e4', 'd4']
-    for i in range(len(first3)):
+    depth = 2
+    while counter < depth:
         if counter == 0:
             firstmove = list(Counter([tuple(first3[i][:1]) for i in range(len(lst))]).items())
             rids.append(firstmove)
             counter = 1
             acceps = [firstmove[i][0] for i in range(len(acceps))]
             acceps = [item for sublist in acceps for item in sublist]
-            print(acceps)
-        if counter == 1:
-            secondmove = list(Counter([tuple(first3[i][:2]) for i in range(len(lst)) if first3[i][0] in acceps]).items())
-            rids.append(secondmove)
-            break
+
+        else:
+            counter+=1
+            othermove = list(Counter([tuple(first3[i][:counter]) for i in range(len(lst)) if first3[i][0] in acceps]).items())
+            rids.append(othermove)
+            zids.append(othermove)
 
 
     r = 0
+    labels = []
+    pz = []
+    true_ids, truu_ids = [], []
     for i in range(len(rids)):
         r += len(rids[i])
+        if i == 0:
+            labels += [z[0][0] for z in firstmove]
+            true_ids = [rids[0][r][0] for r in range(len(rids[0]))]
+            true_ids = [item for sublist in true_ids for item in sublist]
+            firstcount = len(labels)
+        else:
+            labels += [z[0][i] for ply_depth in zids for z in ply_depth]
+            pz += [z[0][:i] for ply_depth in zids for z in ply_depth]
+            true_ids += [rids[x][r][0] for x in range(len(rids)) if x > 0 for r in range(len(rids[x]))]
 
+    print(true_ids)
+
+    parents = ['']*firstcount + [item for sublist in pz for item in sublist] #flattening
+
+    '''
+    labels = what they will show up as
+    parents = exact name of their parents
+    values = completely working
+    ids = their ID is the entire line they are in. e.g. e4 e5 Nf3. Otherwise they will flow between
+    '''
 
     ids = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q']
-    labels = [i[0] for i in firstmove] + [i[0][1] for i in secondmove]# + [i[0][1] for i in thirdmove]
-    parents = [""]*len(firstmove) + [""]*len(secondmove)# + ['a']*len(thirdmove)
-    values = [i[1] for i in firstmove] + [i[1] for i in secondmove]# + [i[1] for i in thirdmove]
+    parents = [""]*len(labels) #keep until solve ID problem
+    values = [i[1] for i in firstmove] + [i[1] for move in zids for i in move]
 
     return ids, labels, parents, values
 
@@ -90,4 +111,4 @@ fig = form(ids, labels, parents, values)
 
 fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
 
-fig.show()
+#fig.show()

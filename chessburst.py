b21a1c76cb0239e177dd1e14e2f3437f0115bc59
Simon <trilogicworlds@gmail.com>
2020-04-26 18:14:57 +0200

    works for depth 2

--- chessburst.py
+++ chessburst.py
@@ -41,18 +41,18 @@ def form(ids, labels, parents, values):
         labels = labels,
         parents = parents,
         values = values,
-        branchvalues = 'total' #if children exceed parent, graph will crash
+        branchvalues = 'total', #if children exceed parent, graph will crash
+        insidetextorientation = 'horizontal'
     ))
     return fig
 
-def form_values():
+def form_values(depth):
 
     first3 = [lst[i][:3] for i in range(len(lst))]
 
     rids, zids = [], []
     counter = 0
     acceps = ['e4', 'd4']
-    depth = 2
     while counter < depth:
         if counter == 0:
             firstmove = list(Counter([tuple(first3[i][:1]) for i in range(len(lst))]).items())
@@ -72,6 +72,8 @@ def form_values():
     labels = []
     pz = []
     true_ids, truu_ids = [], []
+
+
     for i in range(len(rids)):
         r += len(rids[i])
         if i == 0:
@@ -80,35 +82,36 @@ def form_values():
             true_ids = [item for sublist in true_ids for item in sublist]
             firstcount = len(labels)
         else:
-            labels += [z[0][i] for ply_depth in zids for z in ply_depth]
+            #print([z[:i] for ply_depth in zids[i-1] for z in ply_depth if type(z) == tuple]) #== PARENTS
+            labels += [z[i] for ply_depth in zids[i-1] for z in ply_depth if type(z) == tuple]
             pz += [z[0][:i] for ply_depth in zids for z in ply_depth]
             true_ids += [rids[x][r][0] for x in range(len(rids)) if x > 0 for r in range(len(rids[x]))]
 
-    print(true_ids)
 
     parents = ['']*firstcount + [item for sublist in pz for item in sublist] #flattening
+    print(pz, '\n\n\n\n')
+    print(parents)
 
-    '''
-    labels = what they will show up as
-    parents = exact name of their parents
-    values = completely working
-    ids = their ID is the entire line they are in. e.g. e4 e5 Nf3. Otherwise they will flow between
-    '''
-
-    ids = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q']
-    parents = [""]*len(labels) #keep until solve ID problem
+    ids = true_ids
+    #parents = [""]*len(labels) #keep until solve ID problem
     values = [i[1] for i in firstmove] + [i[1] for move in zids for i in move]
-
+    '''
+    print('\n\n', ids)
+    print('\n\n')
+    print(labels)
+    print('\n\n')
+    print(parents, '\n\n', values)
+    '''
     return ids, labels, parents, values
 
 ## NOTE:  for repeated labels, all you have to do is change the ID of the label e.g fig = go.figure. etc... (ids = 'joe', labels = 'what's displayed)
 
 lst = parse_individual_games(mine) #ask for input here
 
-ids, labels, parents, values = form_values()
+ids, labels, parents, values = form_values(2)
 
 fig = form(ids, labels, parents, values)
 
 fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
 
-#fig.show()
+fig.show()

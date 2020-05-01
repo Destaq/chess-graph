5543bb6ae42ad65dd1dc0f3ac6995f5cef5caf65
Simon <trilogicworlds@gmail.com>
2020-05-01 14:04:28 +0200

    update test

--- game_parser_test.py
+++ game_parser_test.py
@@ -3,4 +3,4 @@ import game_parser
 
 def test_parse_individual_games():
     pgn = open("pgns/mir_khan.pgn")
-    assert len(game_parser.parse_individual_games(pgn, 3)) == 156
+    assert len(game_parser.parse_individual_games(pgn, 3, False)) == 156

40ff983f024034d6d47bed13fc883d0828af9dda
Simon <trilogicworlds@gmail.com>
2020-05-02 11:29:53 +0200

    fix workflow fail

--- game_parser_test.py
+++ game_parser_test.py
@@ -3,7 +3,7 @@ import game_parser
 
 def test_parse_individual_games():
     pgn = open("pgns/mir_khan.pgn")
-    lst, ratios = game_parser.parse_individual_games(pgn, 3, False)
+    lst, ratios, kick_depth = game_parser.parse_individual_games(pgn, 3, False)
     assert len(lst) == 156
 
 test_parse_individual_games()

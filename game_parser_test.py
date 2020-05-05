771fbdc6c153741cdba80515069d55252c9da134
Simon <trilogicworlds@gmail.com>
2020-05-05 13:36:54 +0200

    update test

--- game_parser_test.py
+++ game_parser_test.py
@@ -2,8 +2,7 @@ import new_parser
 
 
 def test_parse_individual_games():
-    pgn = open("pgns/mir_khan.pgn")
-    lst, ratios, kick_depth = new_parser.parse_games(pgn, 3, False)
+    lst, ratios, kick_depth = new_parser.parse_games("pgns/mir_khan.pgn", 3, False)
     assert len(lst) == 156
 
 test_parse_individual_games()

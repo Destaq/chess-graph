709721047404c8621c83da7392634e2ce4abdfe6
Simon <trilogicworlds@gmail.com>
2020-05-05 12:51:28 +0200

    fix new parser workflow fail

--- game_parser_test.py
+++ game_parser_test.py
@@ -1,9 +1,9 @@
-import game_parser
+import new_parser
 
 
 def test_parse_individual_games():
     pgn = open("pgns/mir_khan.pgn")
-    lst, ratios, kick_depth = game_parser.parse_individual_games(pgn, 3, False)
+    lst, ratios, kick_depth = new_parser.parse_games(pgn, 3, False)
     assert len(lst) == 156
 
 test_parse_individual_games()
---
+++ new_parser.py
@@ -0,0 +1,43 @@
+import pgn
+import sys
+
+
+def parse_games(database, depth, custom_branching):
+    database = open(database)
+    pgn_text = database.read()
+    database.close()
+
+    games = pgn.loads(pgn_text)
+
+    all_games = []
+    all_ratios = []
+    kick_depth = 0
+
+    for game in games:
+        if len(game.moves) >= depth+2:
+            all_games.append(game.moves)
+
+    for i in range(len(all_games)):
+        all_games[i].pop(-2)
+        all_ratios.append(all_games[i][-1])
+        all_games[i].pop(-1)
+
+    if custom_branching:
+        a = input('Custom Branching: ')
+        a = list(a.split())
+
+        del_list = []
+        kick_depth = len(a)
+
+        for i in range(len(all_games)):
+            b = all_games[i][0:len(a)]
+            if a == b:
+                pass
+            else:
+                del_list.append(i)
+
+
+        all_games = [all_games[i] for i in range(len(all_games)) if i not in del_list]
+        all_ratios = [all_ratios[i] for i in range(len(all_ratios)) if i not in del_list]
+
+    return all_games, all_ratios, kick_depth

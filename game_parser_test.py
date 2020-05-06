7b93492c8dda910d40e5160ef8ddfea97c489a73
Simon <trilogicworlds@gmail.com>
2020-05-06 10:22:47 +0200

    fix game parser test crash

--- game_parser_test.py
+++ game_parser_test.py
@@ -2,7 +2,7 @@ import new_parser
 
 
 def test_parse_individual_games():
-    lst, ratios, kick_depth = new_parser.parse_games("pgns/mir_khan.pgn", 3, False)
+    lst, ratios, kick_depth = new_parser.parse_games("pgns/mir_khan.pgn", 3, False, color = 'both', name = '')
     assert len(lst) == 156
 
 test_parse_individual_games()

48acdbbe6bbf17e091985bace00ad06373b39e30
Simon Ilincev <61620873+Destaq@users.noreply.github.com>
2020-05-14 15:28:21 +0200

    Delete game_parser_test.py

--- game_parser_test.py
+++
@@ -1,9 +0,0 @@
-import new_parser
-
-
-def test_parse_individual_games():
-    lst, ratios, kick_depth = new_parser.parse_games("pgns/mir_khan.pgn", 5, False, color = 'both', name = '')
-    print(len(lst))
-    assert len(lst) == 156
-
-test_parse_individual_games()
